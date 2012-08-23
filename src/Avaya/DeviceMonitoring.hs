e16cca99850f328f00a119f113b425dd335b3210
max taldykin <jorpic@gmail.com>
2012-08-24 02:31:27 +0400

    Device monitoring macrocommand

---
+++ src/Avaya/DeviceMonitoring.hs
@@ -0,0 +1,36 @@
+
+module Avaya.DeviceMonitoring where
+
+import Avaya.MessageLoop
+import qualified Avaya.Messages.Request as Rq
+import qualified Avaya.Messages.Response as Rs
+
+-- TODO: monitoring handler
+--       reset timeout thread
+--       stop monitoring
+-- startDeviceMonitoring :: LoopHandle -> Text -> Text -> IO ()
+startDeviceMonitoring h user pass switch ext pwd = do
+  Rs.StartApplicationSessionPosResponse{..} <- sendRequestSync h
+    $ Rq.StartApplicationSession
+      {applicationId = "Test"
+      ,requestedProtocolVersion = Rq.V5_2
+      ,userName = user
+      ,password = pass
+      ,sessionCleanupDelay = 180
+      ,requestedSessionDuration = 180
+      }
+  Rs.GetDeviceIdResponse{..} <- sendRequestSync h
+    $ Rq.GetDeviceId
+      {switchName = switch
+      ,extension = ext
+      }
+  sendRequestSync h
+    $ Rq.MonitorStart
+      {acceptedProtocol = actualProtocolVersion
+      ,deviceObject = device
+      }
+  sendRequestSync h
+    $ Rq.RegisterTerminalRequest
+      {device = device
+      ,password = pwd
+      }
