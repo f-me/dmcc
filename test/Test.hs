7f646c6765fb9c00860db2572e15ec3b125b71be
Max Taldykin <max@formalmethods.ru>
2012-08-27 14:38:34 +0400

    Test: extension number as argument

--- test/Test.hs
+++ test/Test.hs
@@ -3,13 +3,17 @@
 import Control.Monad
 import Control.Concurrent
 
+import qualified Data.Text as T
+import System.Environment
+
 import Avaya.MessageLoop
 import Avaya.DeviceMonitoring
 
 main :: IO ()
 main = do
+  [ext] <- getArgs
   Right h <- startMessageLoop "127.0.0.1" 4721
   attachObserver h print
   startDeviceMonitoring h
-    "user" "pass" "S8300ADAC" "125" "1234567"
-  forever $ threadDelay 1000
+    "user" "pass" "S8300ADAC" (T.pack ext) "1234567"
+  forever $ threadDelay 1000000
