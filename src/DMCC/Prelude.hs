8f72cb27c84bf3494fb7327b3b093beec9b0aa04
Dmitry Dzhus <dima@dzhus.org>
2017-06-29 16:01:53 +0100

    refactor: monad-logger, custom prelude, LTS-8

---
+++ src/DMCC/Prelude.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module DMCC.Prelude
+  ( module ClassyPrelude
+  , module Control.Concurrent.STM.Lifted
+  , MonadCatchLoggerIO
+  )
+
+where
+
+import ClassyPrelude hiding ( atomically
+                            , newBroadcastTChanIO
+                            , newEmptyTMVarIO
+                            , newTBQueueIO
+                            , newTChanIO
+                            , newTMVarIO
+                            , newTQueueIO
+                            , newTVarIO
+                            , readTVarIO
+                            )
+import Control.Concurrent.STM.Lifted
+import Control.Monad.Logger
+
+type MonadCatchLoggerIO m = (MonadCatch m, MonadLoggerIO m)
